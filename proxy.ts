fcae9811bdb2b77a631833427e811395031d2023
yakovleva2904 <yakovlevaolga2904@gmail.com>
2026-05-19 12:53:10 +0200

    changed in api and proxy

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 const privateRoutes = ['/notes', '/profile'];
 const authRoutes = ['/sign-in', '/sign-up'];
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const accessToken = request.cookies.get('accessToken')?.value;
